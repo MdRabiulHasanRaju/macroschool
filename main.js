c06c9e4b03970bef291eb8723cc87791b83c3163
Md_Rabiul_Hasan <rhr2416@gmail.com>
2024-07-14 02:24:17 +0600

    Course Details Page backend work

--- main.js
+++ main.js
@@ -1,48 +1,52 @@
-window.addEventListener('scroll', () => {
-    document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0)
-  })
-  
-  
-  const menu = document.querySelector(".nav__menu");
-  const menuBtn = document.querySelector("#open-menu-btn");
-  const closeBtn = document.querySelector("#close-menu-btn");
-  
-  menuBtn.addEventListener('click' , () => {
-      menu.style.display = "flex";
-      closeBtn.style.display = "inline-block";
-      menuBtn.style.display = "none";
-  })
-  
-  
-  const closeNav = () =>{
-      menu.style.display = "none";
-      closeBtn.style.display = "none";
-      menuBtn.style.display = "inline-block";
-  }
-  
-  closeBtn.addEventListener('click', closeNav)
-  
-  
-  
-  const videoThumbnails = document.querySelectorAll(".video-gallery .all-videos .thumbnail");
-  
-  const videoPlayer = document.querySelector(".video-gallery .featured-video iframe");
-  
-  const videoTitle = document.querySelector(".video-gallery .video-title");
-  
-  const showVideo = (videoId, title) => {
-       let videoUrl = `https://www.youtube.com/embed/${videoId}?rel=0`;
-       videoPlayer.setAttribute("src", videoUrl);
-       videoTitle.innerHTML = title;
-  };
-  videoThumbnails.forEach(v => {
-      v.addEventListener("click", () => {
-          showVideo(v.dataset.id, v.dataset.title)
-      });
+window.addEventListener("scroll", () => {
+  document
+    .querySelector("nav")
+    .classList.toggle("window-scroll", window.scrollY > 0);
+});
+
+const menu = document.querySelector(".nav__menu");
+const menuBtn = document.querySelector("#open-menu-btn");
+const closeBtn = document.querySelector("#close-menu-btn");
+
+menuBtn.addEventListener("click", () => {
+  menu.style.display = "flex";
+  closeBtn.style.display = "inline-block";
+  menuBtn.style.display = "none";
+});
+
+const closeNav = () => {
+  menu.style.display = "none";
+  closeBtn.style.display = "none";
+  menuBtn.style.display = "inline-block";
+};
+
+closeBtn.addEventListener("click", closeNav);
+
+const videoThumbnails = document.querySelectorAll(
+  ".video-gallery .all-videos .thumbnail"
+);
+
+const videoPlayer = document.querySelector(
+  ".video-gallery .featured-video iframe"
+);
+
+const videoTitle = document.querySelector(".video-gallery .video-title");
+
+const showVideo = (videoId, title) => {
+  let videoUrl = `https://www.youtube.com/embed/${videoId}?rel=0`;
+  videoPlayer.setAttribute("src", videoUrl);
+  videoTitle.innerHTML = title;
+};
+videoThumbnails.forEach((v) => {
+  v.addEventListener("click", () => {
+    showVideo(v.dataset.id, v.dataset.title);
   });
-  
-  function collapse() {
-    var para = document.getElementById("para");
+});
+
+let questionClick = document.querySelectorAll(".questionClick");
+questionClick.forEach((e) => {
+  e.addEventListener("click", () => {
+    para = e.parentNode.childNodes[3];
     para.classList.toggle("collapsed");
-}
-  
+  });
+});
--- views/pages/courses/course-details.php
+++ views/pages/courses/course-details.php
@@ -1,8 +1,26 @@
-<?php
-$title = "Macro School - Courses";
-?>
-<?php
+<?php ob_start();
+session_start();
+// if (isset($_GET['id'])) {
+//     $course_id = $_GET["id"];
+// }
+$course_id = 1;
+require_once $_SERVER['DOCUMENT_ROOT'] . "/macroschool/lib/Database.php";
+
+$title_sql = "select course_title from courses where id=$course_id";
+$title_stmt = mysqli_prepare($connection, $title_sql);
+mysqli_stmt_execute($title_stmt);
+mysqli_stmt_store_result($title_stmt);
+mysqli_stmt_bind_result($title_stmt, $course_title);
+mysqli_stmt_fetch($title_stmt);
+
+$title = "$course_title - Macro School";
+$meta_description = "$title - macro school Call 880 1563 4668 21";
+$meta_keywords = "$title, Macro School, macroschool,macro,schoolmacro,macro";
+$header_active = "Courses";
+
 include("../../inc/header.php");
+
+// include $_SERVER['DOCUMENT_ROOT']."/macroschool/views/pages/courses/admission-form.php";
 ?>
 
 <style>
@@ -15,77 +33,138 @@ include("../../inc/header.php");
     }
 </style>
 
-<section class="container course-details">
-    <div class="course-details-top">
-        <h1>Macro School Shop</h1>
-        <h2>Macro School Varsity Special Private Programme 2024</h2>
-    </div>
-    <div class="course-details-col">
-        <div class="course-details-col-1">
-            <h3>Faculties</h3>
-            <div class="course-details-faculties">
-                <div class="faculti-member">
-                    <img src="public/images/sajjad.jpg" id="faculti-image">
-                    <h5 class="teacher-name">Sajjad Alom</h5>
-                    <h5 class="teacher-department">Physics</h5>
+<?php
+$sql = "select * from courses where id=?";
+$stmt = mysqli_prepare($connection, $sql);
+mysqli_stmt_bind_param($stmt, "i", $param_id);
+$param_id = $course_id;
+if (mysqli_stmt_execute($stmt)) {
+    mysqli_stmt_store_result($stmt);
+    if (mysqli_stmt_num_rows($stmt) == 0) {
+        header("location: " . LINK . "error/404");
+        die();
+    } else {
+        mysqli_stmt_bind_result(
+            $stmt,
+            $id,
+            $cat_id,
+            $faculties,
+            $course_title,
+            $course_details,
+            $faq,
+            $image,
+            $total_student,
+            $deadline,
+            $course_info,
+            $routine,
+            $regular_price,
+            $offer_price
+        );
+        if (mysqli_stmt_fetch($stmt)) { ?>
+            <section class="container course-details">
+                <div class="course-details-top">
+                    <h1>Macro School Shop</h1>
+                    <h2><?= $course_title; ?></h2>
                 </div>
-                <div class="faculti-member">
-                    <img src="public/images/sajjad.jpg" id="faculti-image">
-                    <h5 class="teacher-name">Sajjad Alom</h5>
-                    <h5 class="teacher-department">Physics</h5>
-                </div>
-                <div class="faculti-member">
-                    <img src="public/images/sajjad.jpg" id="faculti-image">
-                    <h5 class="teacher-name">Sajjad Alom</h5>
-                    <h5 class="teacher-department">Physics</h5>
-                </div>
-            </div>
-            <div class="course-details-about">
-                <h3>কোর্সটি সম্পর্কে</h3>
-                <p>আমাদের ছাত্র-ছাত্রীদের মধ্যে একটা ধারণা তৈরি হয়েছে যে, ভার্সিটিতে চান্স পেতে হলে ইঞ্জিনিয়ারিং বা মেডিকেল এর প্রস্তুতি থাকলে ভাল, কেননা সেখানে ডিপলি পড়ানো হয়৷ এই ধারণাটা ভুল নয়, কেননা বিভিন্ন জায়গায় ভার্সিটির পড়ানোগুলো surface লেভেলের হয়ে থাকে৷ তবে ACS এ এবার আমরা এই ধারণাকে পরিবর্তন করতে চাই। কারো যদি ভার্সিটি টার্গেট থাকে সে যেনো কেবল এখানে প্রস্তুতি নিয়েই চান্স পায় এরকম ব্যবস্থা করছি৷</p>
-            </div>
-            <div class="course-details-faq">
-                <h3>সচরাচর জিজ্ঞাসা :</h3>
-                <div class="course-details-faq-box">
-                    <p onclick="collapse()"><img class="icon" src="public/images/icon/dropdown.png">কোর্সটি কীভাবে কিনবো?</p>
-                    <div class="para" id="para">
-                        <p>বি.দ্র. কেনার পূর্বে অবশ্যই এই ভিডিওটি দেখে নাও : </p>
-                        <a href="https://www.youtube.com/watch?v=ji7epk_R20U" target="_blank" rel="noopener noreferrer">https://www.youtube.com/watch?v=ji7epk_R20U</a>
+                <div class="course-details-col">
+                    <div class="course-details-col-1">
+                        <h3>Faculties</h3>
+                        <div class="course-details-faculties">
+                            <?php
+                            $faculties = explode(',', $faculties);
+                            foreach ($faculties as $member_id) {
+                                $faculties_sql = "select * from faculties where id=$member_id";
+                                $faculties_stmt = mysqli_prepare($connection, $faculties_sql);
+                                mysqli_stmt_execute($faculties_stmt);
+                                mysqli_stmt_store_result($faculties_stmt);
+                                mysqli_stmt_bind_result($faculties_stmt, $id, $name, $department, $image);
+                                mysqli_stmt_fetch($faculties_stmt); ?>
+                                <div class="faculti-member">
+                                    <img src="public/images/<?= $image; ?>" id="faculti-image">
+                                    <h5 class="teacher-name"><?= $name; ?></h5>
+                                    <h5 class="teacher-department"><?= $department; ?></h5>
+                                </div>
+                            <?php } ?>
+                        </div>
+                        <div class="course-details-about">
+                            <h3>কোর্সটি সম্পর্কে</h3>
+                            <p><?= $course_details; ?></p>
+                        </div>
+                        <div class="course-details-faq">
+                            <h3>সচরাচর জিজ্ঞাসা :</h3>
+                            <?php
+                            $faq = explode(',', $faq);
+                            foreach ($faq as $faq_id) {
+                                $faq_sql = "select * from faq where id=$faq_id";
+                                $faq_stmt = mysqli_prepare($connection, $faq_sql);
+                                mysqli_stmt_execute($faq_stmt);
+                                mysqli_stmt_store_result($faq_stmt);
+                                mysqli_stmt_bind_result($faq_stmt, $id, $question, $answer);
+                                mysqli_stmt_fetch($faq_stmt); ?>
+                                <div class="course-details-faq-box">
+                                    <p class="questionClick"><img class="icon" src="public/images/icon/dropdown.png"><?= $question; ?></p>
+                                    <div class="para" id="para">
+                                        <?= $answer; ?>
+                                    </div>
+                                </div>
+                            <?php } ?>
+                        </div>
                     </div>
+                    <div class="course-details-col-2">
+                        <div class="course-box">
+                            <img src="public/images/Hm course 2.jpg">
+                            <div class="course-box-top">
+                                <div class="total">
+                                    <img src="public/images/icon/people.png" alt="" class="icon">
+                                    <p>কোর্সটি করছেন <strong><?= $total_student; ?></strong></p>
+                                </div>
+                                <div class="time">
+                                    <img src="public/images/icon/time.png" alt="" class="icon">
+                                    <p><?= $deadline; ?></p>
+                                </div>
+                            </div>
+                            <div class="course-box-middle">
+                                <?php
+                                $course_info = explode(',', $course_info);
+                                foreach ($course_info as $course_info_id) {
+                                    $course_info_sql = "select * from course_info where id=$course_info_id";
+                                    $course_info_stmt = mysqli_prepare($connection, $course_info_sql);
+                                    mysqli_stmt_execute($course_info_stmt);
+                                    mysqli_stmt_store_result($course_info_stmt);
+                                    mysqli_stmt_bind_result($course_info_stmt, $id, $title, $number);
+                                    mysqli_stmt_fetch($course_info_stmt); ?>
+                                    <p><img src="public/images/icon/ok.png" alt="" class="icon"> <?= $title; ?> <?= $number; ?></p>
+                                <?php } ?>
+                            </div>
+                            <div class="course-box-bottom">
+                                <a href="<?= $routine; ?>" class="my-btn">Download Class Routine</a>
+                            </div>
+                        </div>
+                        <div class="price-box">
+                            <h3>Price: <del style="color:red"><?= $regular_price; ?>৳</del> <span style="color:green"><?= $offer_price; ?>৳</span></h3>
+                            <div class="course-box-bottom">
+                                <?php
+                                if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) { ?>
+                                    <button class="my-btn green">কোর্সটিতে এনরোল করো</button>
+                                <?php } else { ?>
+                                    <a href="<?= LINK; ?>admission">
+                                        <button class="my-btn green">কোর্স কিনতে লগইন করো</button>
+                                    </a>
+                                <?php } ?>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+<?php
+        }
+    }
+}
+?>
 
-                </div>
-            </div>
-        </div>
-        <div class="course-details-col-2">
-            <div class="course-box">
-                <img src="public/images/Hm course 2.jpg">
-                <div class="course-box-top">
-                    <div class="total">
-                    <img src="public/images/icon/people.png" alt="" class="icon">
-                    <p>কোর্সটি করছেন <strong>5,000</strong></p>
-                    </div>
-                    <div class="time">
-                    <img src="public/images/icon/time.png" alt="" class="icon">
-                    <p>২৪ এর ডেন্টাল পরীক্ষা পর্যন্ত</p>
-                    </div>
-
-                </div>
-                <div class="course-box-middle">
-                    <p><img src="public/images/icon/ok.png" alt="" class="icon"> ক্লাস সংখ্যা ১২০+</p>
-                    <p><img src="public/images/icon/ok.png" alt="" class="icon"> ক্লাস সংখ্যা ১২০+</p>
-                    <p><img src="public/images/icon/ok.png" alt="" class="icon"> ক্লাস সংখ্যা ১২০+</p>
-                    <p><img src="public/images/icon/ok.png" alt="" class="icon"> ক্লাস সংখ্যা ১২০+</p>
-                    <p><img src="public/images/icon/ok.png" alt="" class="icon"> ক্লাস সংখ্যা ১২০+</p>
-                </div>
-                <div class="course-box-bottom">
-                    <Button class="my-btn">Download Class Routine</Button>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
 <?php
 include("../../inc/footer.php");
+ob_end_flush();
 ?>
 <script src="<?= LINK; ?>main.js"></script>
 </body>
